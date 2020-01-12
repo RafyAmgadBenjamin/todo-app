06138f4744aefa1088ccff7e458cf4ca482be97b
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-01-12 09:59:35 +0200

    Instantiate the migration

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import sys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://rafy:admin@localhost:5432/todoapp"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # migrations instantiation
 
 
 class Todo(db.Model):
